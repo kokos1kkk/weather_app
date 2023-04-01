654c52a0eca8ae5b29008b0da0a45f711d361ad6
Killitar <78868798+Killitar@users.noreply.github.com>
2023-04-01 15:29:28 +0300

    Add files via upload

--- project/front/script.js
+++ project/front/script.js
@@ -8,5 +8,29 @@ let getWeather = () => {
   if (cityValue.length == 0) {
     result.innerHTML = `<h3 class="msg">Please enter a city name</h3>`;
   } else {
+    eel.get_weather(cityValue)(function (data) {
+      result.innerHTML = `
+        <h2>${data.name}</h2>
+        <h4 class="weather">${data.weather[0].main}</h4>
+        <h4 class="desc">${data.weather[0].description}</h4>
+        <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png">
+        <h1>${data.main.temp} &#176;</h1>
+        <div class="temp-container">
+            <div>
+                <h4 class="title">min</h4>
+                <h4 class="temp">${data.main.temp_min}&#176;</h4>
+            </div>
+            <div>
+                <h4 class="title">max</h4>
+                <h4 class="temp">${data.main.temp_max}&#176;</h4>
+            </div>
+        </div>
+        `;
+
+      cityRef.value = "";
+    });
   }
 };
+
+searchBtn.addEventListener("click", getWeather);
+window.addEventListener("load", getWeather);
--- project/main.py
+++ project/main.py
@@ -5,13 +5,13 @@ import requests;
 def get_weather(city):
     api_key = "9341597124d912b5669e6a98711aab2b"
     
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&lang=ru'
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric&lang=ru'
     
     response = requests.get(url)
     
     if response.status_code == 200:
-        data = response.json()
-        return data
+        return response.json()
+        
     else:
         print('error')
 
