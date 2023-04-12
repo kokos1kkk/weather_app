67ea7e87781e46ffd621a88e73a839a2e4293f6e
kokos1kkk <120467152+kokos1kkk@users.noreply.github.com>
2023-04-12 22:45:49 +0300

    Update main.py

--- project/main.py
+++ project/main.py
@@ -5,7 +5,11 @@ import requests;
 def get_weather(city):
     api_key = "9341597124d912b5669e6a98711aab2b"
     
-    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric&lang=ru'
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric&lang=ru' #api для точной погоды сейчас
+    
+    #https://openweathermap.org/guide - тут смотреть api
+    
+    gpm = f'https://maps.openweathermap.org/maps/2.0/radar/6/13/24?&appid={api_key}&tm=1600781400' #api для карты осадков хз работает или не
     
     response = requests.get(url)
     
