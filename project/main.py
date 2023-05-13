2d5508fa15bdbb1280b60efb5c1d67c15715a338
kokos1kkk <120467152+kokos1kkk@users.noreply.github.com>
2023-05-13 09:31:10 +0300

    Update main.py

--- project/main.py
+++ project/main.py
@@ -6,7 +6,10 @@ def get_weather(city):
     api_key = "9341597124d912b5669e6a98711aab2b"
     
     url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric&lang=ru'
-    
+    url1 = f'http://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={api_key}&lang=ru'
+    mapurl = f'https://maps.openweathermap.org/maps/2.0/radar/{z}/{x}/{y}?appid={api_key}&tm={date}&lang=ru'
+    airurl = f'http://api.openweathermap.org/data/2.5/air_pollution?lat={lat}&lon={lon}&appid={api_key}&lang=ru'
+
     a = 1
     response = requests.get(url)
     
